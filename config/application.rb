7edc1fe1c2685a44d8792152b9df68b468af015b
watsumi <w.utsumi2@gmail.com>
2022-04-30 15:24:47 +0900

    i18n対応

--- config/application.rb
+++ config/application.rb
@@ -33,5 +33,7 @@ module OldJanken
 
     # Don't generate system test files.
     config.generators.system_tests = nil
+    config.time_zone = "Tokyo"
+    config.i18n.default_locale = :ja
   end
 end
