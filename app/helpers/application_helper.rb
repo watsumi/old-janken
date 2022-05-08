077a92c8c9aefca9c2a9683ad69d1bf0c94d3b64
watsumi <w.utsumi2@gmail.com>
2022-05-08 13:09:55 +0900

    helper修正

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,17 @@
 module ApplicationHelper
+  def icon(icon_name)
+    tag.i(class: ["bi", "bi-#{icon_name}"])
+  end
+
+  def icon_with_text(icon_name, text)
+    tag.span(icon(icon_name), class: "me-2") + tag.span(text)
+  end
+
+  def turbo_stream_flash
+    turbo_stream.append "flashes", partial: "flash"
+  end
+
+  def turbo_stream_background
+    turbo_stream.replace "background", partial: "background"
+  end
 end
---
+++ app/helpers/users_helper.rb
@@ -0,0 +1,9 @@
+module UsersHelper
+  def is_enemy?(user)
+    if current_user&.host_role?
+      user.guest_role?
+    elsif current_user&.guest_role?
+      user.host_role?
+    end
+  end
+end
