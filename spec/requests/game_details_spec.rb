62fa097a8a600c18ddcd4372d35615cfd4592518
watsumi <w.utsumi2@gmail.com>
2022-05-14 00:25:11 +0900

    request spec修正

--- spec/requests/game_details_spec.rb
+++ spec/requests/game_details_spec.rb
@@ -2,7 +2,7 @@ require 'rails_helper'
 
 RSpec.describe 'GameDetails', type: :request do
   let(:game) { create(:game) }
-  let(:game_detail) { create_list(:game_detail, 6, game:, user:) }
+  let!(:game_detail) { create_list(:game_detail, 6, game:, user:) }
   let(:user) { create(:user, game:, role: :host) }
   before do
     allow_any_instance_of(ApplicationController).to receive(:current_user).and_return(user)
