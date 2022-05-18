ce4e2abf35c50f592f55b951c611d76aba16dd79
watsumi <w.utsumi2@gmail.com>
2022-05-18 23:14:10 +0900

    offcanvasの制御を修正

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -33,8 +33,8 @@ module ApplicationHelper
     }
   end
 
-  def is_game_page?
-    !(current_page?(root_path) || current_page?(rules_path) || current_page?(terms_path) || current_page?(credits_path) || current_page?(privacy_policy_path))
+  def is_game_detail_page?
+    !(current_page?(root_path) || current_page?(rules_path) || current_page?(terms_path) || current_page?(credits_path) || current_page?(privacy_policy_path) | current_page?(games_path) | current_page?(paticipates_game_path))
   end
 
   def icon(icon_name)
