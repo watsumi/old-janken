0d1350acd1d68c2a950db2e426bd668b6e68d838
watsumi <w.utsumi2@gmail.com>
2022-05-29 11:08:40 +0900

    allow_any_instance_ofをリファクタ

--- spec/requests/games_spec.rb
+++ spec/requests/games_spec.rb
@@ -1,9 +1,24 @@
 require 'rails_helper'
 
 RSpec.describe '/games', type: :request do
+  let(:game) { create(:game) }
+  let(:host) { create(:user, game:, role: :host, character: Character.all.sample) }
+  let(:guest) { create(:user, game:, role: :guest, character: Character.all.sample) }
+  let!(:host_game_detail) { create_list(:game_detail, 3, game:, user: host) }
+  let!(:guest_game_detail) { create_list(:game_detail, 3, game:, user: guest) }
+
+  before { login_as(host) }
+
   describe 'GET /index' do
     it 'renders a successful response' do
-      get root_path
+      get games_path
+      expect(response).to be_successful
+    end
+  end
+  describe 'GET /show' do
+    let(:game) { create(:game) }
+    it 'renders a successful response' do
+      get game_url(game)
       expect(response).to be_successful
     end
   end
