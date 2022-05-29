ef3beaf2c960f7c24758720089b870b414aaebb5
watsumi <w.utsumi2@gmail.com>
2022-05-29 11:34:09 +0900

    users_spec追加

--- spec/requests/users_spec.rb
+++ spec/requests/users_spec.rb
@@ -1,4 +1,28 @@
 require 'rails_helper'
 
 RSpec.describe '/users', type: :request do
+  let(:game) { create(:game) }
+  let(:host)  { create(:user, game: game, role: :host) }
+  let(:guest) { create(:user, game: game, role: :guest) }
+  let!(:game_details) do
+    create(:game_detail, game: game, user: host, hand_id: 1, turn: :host_turn_1)
+    create(:game_detail, game: game, user: guest, hand_id: 2, turn: :guest_turn_1)
+    create(:game_detail, game: game, user: host, hand_id: 1, turn: :host_turn_2)
+  end
+
+  before { login_as(host) }
+
+  describe 'GET /edit/:user_id' do
+    it 'renders a successful response' do
+      get edit_user_path(host)
+      expect(response).to be_successful
+    end
+  end
+
+  describe 'POST /users/:user_id/authorize' do
+    it 'renders a successful response' do
+      post user_authorize_path(host, game_id: game.id)
+      expect(response).to redirect_to(game_path(game))
+    end
+  end
 end
